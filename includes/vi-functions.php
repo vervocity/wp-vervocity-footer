329b51edfe2c7fa88cd7edfc8a3b1f46e00575d9
Kayla Hageman <khageman@vervocity.io>
2021-02-17 10:15:54 -0600

    Update vi-functions.php

--- includes/vi-functions.php
+++ includes/vi-functions.php
@@ -17,7 +17,7 @@ function vi_footer_copyright() {
     $output = '';
     $output .= '<div class="vi-footer">';
       $output .= '<div class="copyright"> © '. date('Y') .' '.$site_title.'</div>';
-      $output .= '<div class="developed-by"><a target="_blank" title="Site Developed Vervocity" href="https://vervocity.io">
+      $output .= '<div class="developed-by"><a target="_blank" title="Site Developed by Vervocity" href="https://vervocity.io">
         <img alt="Site Developed by Vervocity" src="'.plugin_dir_url( dirname( __FILE__ )).'public/images/vervocity-logo-100x25.png" /></a></div>';
     $output .= '</div>';
 
